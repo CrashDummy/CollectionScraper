c8d43fa5c9710d3122140ca5e5651bb7e5513677
anthonywu <anthony.h.wu@gmail.com>
2020-07-12 12:15:21 +1000

    Updated with csv export

--- fftcg.py
+++ fftcg.py
@@ -1,24 +1,29 @@
-from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException
+import logging
+import sys
+import time
+import csv
+import copy
+
+from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
-from bs4 import BeautifulSoup
-import time
-import os
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def getdriver(headless=True):
+    driverpath = '/opt/WebDriver/bin/chromedriver'
+
     if headless:
         opts = Options()
         opts.headless = True
         assert opts.headless  # Operating in headless mode
-        driver = Chrome(options=opts, executable_path='/opt/WebDriver/bin/chromedriver')
+        driver = Chrome(options=opts, executable_path=driverpath)
     else:
-        driver = Chrome(executable_path='/opt/WebDriver/bin/chromedriver')
+        driver = Chrome(executable_path=driverpath)
     return driver
 
 
@@ -88,23 +93,25 @@ def clickXPath_fast(driver, xpath):
         EC.element_to_be_clickable((By.XPATH, xpath)))
     try:
         button.click()
-    except StaleElementReferenceException as Exception:
-        print('StaleElementReferenceException while trying click the button' + xpath + ', trying to find element again')
+    except StaleElementReferenceException as ex:
+        logging.debug('Exception ' + str(ex) + ' while trying click the button' + xpath +
+                      ', trying to find element again')
         button = driver.find_element_by_xpath(xpath)
         button.click()
     return button
 
 
 def clickXPath(driver, xpath):
-    print('sleep 5 before searching')
+    logging.info('sleep 5 before searching')
     time.sleep(5)
     button = driver.find_element_by_xpath(xpath)
     try:
-        print('clicking button ' + xpath)
+        logging.info('clicking button ' + xpath)
         button.click()
 
     except NoSuchElementException or StaleElementReferenceException as ex:
-        print('Exception ' + str(ex) + ' while trying click the button' + xpath + ', trying to find element again')
+        logging.debug('Exception ' + str(ex) + ' while trying click the button' + xpath +
+                      ', trying to find element again')
         button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, xpath)))
 
@@ -113,12 +120,15 @@ def clickXPath(driver, xpath):
             .until(EC.presence_of_element_located((By.XPATH, xpath)))
         button.click()
     except TimeoutException as ex:
-        print('Exception ' + str(ex) + ' while trying click the button' + xpath + ', trying to find element again')
+        logging.debug('Exception ' + str(ex) + ' while trying click the button' + xpath +
+                      ', trying to find element again')
         driver.quit()
         exit()
 
     return button
 
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 url = 'https://fftcg.square-enix-games.com/en/card-browser'
 base_url = 'https://fftcg.square-enix-games.com/en/'
@@ -126,57 +136,66 @@ base_url = 'https://fftcg.square-enix-games.com/en/'
 # get web driver
 driver = getdriver(headless=True)
 
-print('getting url')
+logging.info('getting url')
 driver.get(url)
-print('max window')
+logging.info('max window')
 driver.maximize_window()
 # wait for the page to load
 # find and click the search button
-# <button type="submit" class="noselect search-btn"><span class="icon fas fa-search"></span>Search</button>
-# //*[@id="browser"]/div[1]/div[3]/button
-# <span class="icon fas fa-search"></span>
-# submit_button = WebDriverWait(driver, 10).until(
-#    EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]/span[@class="icon fas fa-search"]')))
-# submit_button = driver.find_element_by_xpath('//button[@type="submit"]/span[@class="icon fas fa-search"]')
-# submit_button.click()
 
-print('seting up search button')
+logging.info('setting up search button')
 submit_button_xpath = '//button[@type="submit"]/span[@class="icon fas fa-search"]'
 submit_button = clickXPath(driver, submit_button_xpath)
 
 # find and click on the first element
-# firstelement_button = WebDriverWait(driver, 10).until(
-#    EC.element_to_be_clickable((By.XPATH, '//*[@id="browser"]/div[3]/div[2]')))
-# driver.implicitly_wait(10)
-# firstelement_button.click()
-
-print('seting up first element button')
+logging.info('setting up first element button')
 firstelement_button_xpath = '//*[@id="browser"]/div[3]/div[2]'
 firstelement_button = clickXPath(driver, firstelement_button_xpath)
 
 # find the next button
-# next_button = WebDriverWait(driver, 10).until(
-#    EC.element_to_be_clickable((By.XPATH, '//*[@id="browser"]/div[4]/div[1]/span[3]')))
-
-print('seting up next button')
+logging.info('setting up next button')
 next_button_xpath = '//*[@id="browser"]/div[4]/div[1]/span[3]'
 
 # this is the first card's page
 card_page = BeautifulSoup(driver.page_source, 'html.parser')
 num_cards = int(getTotalCards(card_page))
-card_list = {}
+card_list = []
+
 
 for i in range(num_cards):
     # cook soup with driver
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    card_list['Title'] = getCardTitle(soup)
-    card_list['Image']= getCardImg(soup)
-    card_list.update(getCardText(soup))
+
+    # setup new dictionary for the current card
+    card = {}
+    card['Title'] = getCardTitle(soup)
+    card['Image_full'] = getCardImg(soup)
+    card['Image_thumb'] = card['Image_full'].replace('full', 'thumb')
+    card.update(getCardText(soup))
+
+    # copy dictionary by value (default is by reference)
+    card_list.append(copy.deepcopy(card))
 
     # print(getCardTitle(soup) + " " + getCardText(soup)['Code'])
-    print(card_list['Title'], card_list['Code'])
+    logging.info(card['Title'], card['Code'])
+
     # click for next card
     next_button = clickXPath_fast(driver, next_button_xpath)
+    time.sleep(0.5)
+
+# write data to csv file
+csv_columns = ['Title', 'Image_full', 'Image_thumb', 'Type', 'Job', 'Element', 'Cost', 'Rarity', 'Power', 'Category',
+               'Set', 'Code']
+csv_file = "ff_card_list.csv"
+try:
+    with open(csv_file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for each_card in card_list:
+            writer.writerow(each_card)
+except IOError:
+    logging.error("I/O error")
+
 
 # end the Selenium browser session
 driver.quit()
