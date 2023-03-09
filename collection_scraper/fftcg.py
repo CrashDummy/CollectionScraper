1622cbe32a376fc3a1b07bd691073d1df77b2f4d
Anthony Wu <anthony.h.wu@gmail.com>
2023-03-09 21:43:18 +1100

    Refactored code to remove styling errors

--- collection_scraper/fftcg.py
+++ collection_scraper/fftcg.py
@@ -17,53 +17,61 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def getdriver(headless=True):
-    # Setup Chrome browser for selenium
+def get_driver_path():
     if sys.platform == 'win32':
         driver_path = os.path.join('C:', 'Program Files', 'chromedriver.exe')
     elif sys.platform in ['darwin', 'linux']:
         driver_path = os.path.join('/', 'usr', 'local', 'bin', 'chromedriver')
     else:
         exit()
+    return driver_path
 
+
+def get_chrome_options(headless=True):
     opts = Options()
     if headless:
         opts.add_argument("--headless")  # Operating in headless mode
     else:
         opts.add_argument("--start-maximized")  # max windows
-    driver = Chrome(options=opts, executable_path=driver_path)
-    return driver
+    return opts
 
 
-def driverOpenTab(soup_driver):
+def get_driver(headless=True):
+    driver_path = get_driver_path()
+    opts = get_chrome_options(headless)
+    soup_driver = Chrome(options=opts, executable_path=driver_path)
+    return soup_driver
+
+
+def driver_open_tab(soup_driver):
     # open tab
     # You can use (Keys.CONTROL + 't') on other OSs
     soup_driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.COMMAND + 't')
     return
 
 
-def driverCloseTab(soup_driver):
+def driver_close_tab(soup_driver):
     # close the tab
     # (Keys.CONTROL + 'w') on other OSs.
     soup_driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.COMMAND + 'w')
     return
 
 
-def writeToHTML(filename, contents):
+def write_html(filename, contents):
     # helper function, dump browser's current html content to file
-    with open(filename + ".html", "w") as file:
-        file.write(str(contents))
+    with open(filename + ".html", "w") as html_file:
+        html_file.write(str(contents))
     return
 
 
-def getCardImg(link):
+def get_card_img(link):
     # fftcg page specific - find image function
-    imgs = link.find_all('div', {'class': 'col image'})
-    imglink = imgs[0].find('img').get('src')
-    return imglink
+    images = link.find_all('div', {'class': 'col image'})
+    image_link = images[0].find('img').get('src')
+    return image_link
 
 
-def getCardText(link):
+def get_card_text(link):
     # fftcg page specific - find text function
     card_dict = {}
 
@@ -76,29 +84,29 @@ def getCardText(link):
     img_text = [item.getText().split(':') for item in elements[0].find_all('tr')]
 
     # convert list to dictionary
-    for i in img_text:
-        card_dict.update({i[0]: i[1]})
+    for item in img_text:
+        card_dict.update({item[0]: item[1]})
 
     # find all 'class with name starting with 'icon'
-    eletype = elements[0].findAll("span", {"class": lambda L: L and L.startswith('icon ')})
+    element_type = elements[0].findAll("span", {"class": lambda l: l and l.startswith('icon ')})
 
-    # eletype is a dictionary, simply get the 'class' and element [1]
+    # element_type is a dictionary, simply get the 'class' and element [1]
     # element could be empty
     try:
-        card_dict['Element'] = eletype[0].get('class')[1]
+        card_dict['Element'] = element_type[0].get('class')[1]
     except IndexError as ex:
         logging.debug('Exception ' + str(ex))
         card_dict['Element'] = ""
     return card_dict
 
 
-def getCardTitle(link):
+def get_card_title(link):
     # fftcg page specific - find card title function
     img_title = link.findAll('span', {'class': 'title'})[0].getText()
     return img_title
 
 
-def getTotalCards(link):
+def get_total_cards(link):
     # fftcg page specific - find title function
     total_cards = link.findAll('span', {'class': 'num'})[0].getText()
     # split text by "/"
@@ -106,25 +114,25 @@ def getTotalCards(link):
     return total_cards
 
 
-def clickXPath_fast(driver, xpath):
+def click_xpath_fast(soup_driver, xpath):
     # click on an element on a website, without delay
-    button = WebDriverWait(driver, 10).until(
+    button = WebDriverWait(soup_driver, 10).until(
         ec.element_to_be_clickable((By.XPATH, xpath)))
     try:
         button.click()
     except StaleElementReferenceException as ex:
         logging.debug('Exception ' + str(ex) + ' while trying click the button' + xpath +
                       ', trying to find element again')
-        button = driver.find_element(By.XPATH, xpath)
+        button = soup_driver.find_element(By.XPATH, xpath)
         button.click()
     return button
 
 
-def clickXPath(driver, xpath):
+def click_xpath(soup_driver, xpath):
     # click on an element on a website, with delay
     logging.info('sleep 5 before searching')
     time.sleep(5)
-    button = driver.find_element(By.XPATH, xpath)
+    button = soup_driver.find_element(By.XPATH, xpath)
     try:
         logging.info('clicking button ' + xpath)
         button.click()
@@ -132,22 +140,22 @@ def clickXPath(driver, xpath):
     except NoSuchElementException or StaleElementReferenceException as ex:
         logging.debug('Exception ' + str(ex) + ' while trying click the button' + xpath +
                       ', trying to find element again')
-        button = WebDriverWait(driver, 10).until(
-            ec.element_to_be_clickable((By.XPATH, xpath)))
+#        button = WebDriverWait(soup_driver, 10).until(
+#            ec.element_to_be_clickable((By.XPATH, xpath)))
 
         ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
-        button = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions) \
+        button = WebDriverWait(soup_driver, 10, ignored_exceptions=ignored_exceptions) \
             .until(ec.presence_of_element_located((By.XPATH, xpath)))
         button.click()
     except TimeoutException as ex:
         logging.debug('Exception ' + str(ex) + ' while trying click the button' + xpath +
                       ', trying to find element again')
-        driver.quit()
+        soup_driver.quit()
 
     except ElementClickInterceptedException as ex:
         logging.debug('Exception ' + str(ex) + ' while trying click the button' + xpath +
                       ', element not clickable')
-        driver.quit()
+        soup_driver.quit()
         exit()
 
     return button
@@ -160,7 +168,7 @@ url = 'https://fftcg.square-enix-games.com/en/card-browser'
 base_url = 'https://fftcg.square-enix-games.com/en/'
 
 # get web driver
-driver = getdriver(headless=False)
+driver = get_driver(headless=False)
 
 logging.info('getting url')
 driver.get(url)
@@ -172,13 +180,13 @@ driver.maximize_window()
 logging.info('setting up search button')
 # TODO Implement auto find xpath for search button
 submit_button_xpath = '/html/body/section/section/div/div[2]/div/div[1]/div[3]/button'
-submit_button = clickXPath(driver, submit_button_xpath)
+submit_button = click_xpath(driver, submit_button_xpath)
 
 # find and click on the first element
 logging.info('setting up first element button')
 # time.sleep(1.0)
-firstelement_button_xpath = '//*[@id="browser"]/div[3]/div[2]'
-firstelement_button = clickXPath(driver, firstelement_button_xpath)
+first_element_button_xpath = '//*[@id="browser"]/div[3]/div[2]'
+first_element_button = click_xpath(driver, first_element_button_xpath)
 
 # find the next button
 logging.info('setting up next button')
@@ -186,7 +194,7 @@ next_button_xpath = '//*[@id="browser"]/div[4]/div[1]/span[3]'
 
 # this is the first card's page
 card_page = BeautifulSoup(driver.page_source, 'html.parser')
-num_cards = int(getTotalCards(card_page))
+num_cards = int(get_total_cards(card_page))
 card_list = []
 card = {}
 
@@ -195,10 +203,10 @@ for i in range(num_cards):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     # setup new dictionary for the current card
-    card['Title'] = getCardTitle(soup)
-    card['Image_full'] = getCardImg(soup)
+    card['Title'] = get_card_title(soup)
+    card['Image_full'] = get_card_img(soup)
     card['Image_thumb'] = card['Image_full'].replace('full', 'thumb')
-    card.update(getCardText(soup))
+    card.update(get_card_text(soup))
 
     # copy dictionary by value using copy.deepcopy (default is by reference)
     card_list.append(copy.deepcopy(card))
@@ -211,7 +219,7 @@ for i in range(num_cards):
         file.write(json.dumps(card_list))  # use `json.loads` to do the reverse
 
     # click for next card
-    next_button = clickXPath_fast(driver, next_button_xpath)
+    next_button = click_xpath_fast(driver, next_button_xpath)
     time.sleep(0.5)
 
 # write data to csv file
--- tests/test_getdriver.py
+++ tests/test_getdriver.py
@@ -1,18 +1,27 @@
 import unittest
-from selenium.webdriver import Chrome
-from collection_scraper.fftcg import getdriver
+import os
+from unittest.mock import patch
+from selenium.webdriver.chrome.options import Options
+from collection_scraper.fftcg import get_driver, get_driver_path, get_chrome_options
 
 
 class TestGetDriver(unittest.TestCase):
-    def test_headless(self):
-        driver = getdriver(headless=True)
-        self.assertIsInstance(driver, Chrome)
-        driver.quit()
+    @patch('selenium.webdriver.Chrome')
+    def test_get_driver(self, mock_chrome):
+        # Test that get_driver function returns a Chrome driver instance
+        driver = get_driver()
+        self.assertIsInstance(driver, type(mock_chrome.return_value))
 
-    def test_not_headless(self):
-        driver = getdriver(headless=False)
-        self.assertIsInstance(driver, Chrome)
-        driver.quit()
+    def test_get_driver_path(self):
+        # Test that get_driver_path function returns a valid path
+        driver_path = get_driver_path()
+        self.assertTrue(os.path.exists(driver_path))
+
+    def test_get_chrome_options(self):
+        # Test that get_chrome_options function returns an Options instance with the correct arguments
+        opts = get_chrome_options()
+        self.assertIsInstance(opts, Options)
+        self.assertIn('--headless', opts.arguments)
 
 
 if __name__ == '__main__':
